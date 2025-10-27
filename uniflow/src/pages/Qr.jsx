aee282ec025db53419b82c5ffedeb9f5ec58f451
Heshan904 <heshanhasitha904@gmail.com>
2025-10-27 22:02:43 +0530

    fixed

---
+++ uniflow/src/pages/Qr.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import NaviBar from '../components/NaviBar'
+import SideBar from '../components/SideBar'
+
+
+function Qr() {
+  return (
+    <>
+    <NaviBar/>
+        <div className="dash">
+          <SideBar/>
+          <div className="nimg">
+            <h1>QR Reader</h1>
+            
+            
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Qr
